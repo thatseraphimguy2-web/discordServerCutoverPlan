8d71bad9511ede56e3acde4bada423e670e18f74
Matthew Prince <matthew.prince@awanuigroup.co.nz>
2026-08-31 13:50:10 +1200

    Initial commit

---
+++ command.js
@@ -0,0 +1,22 @@
+//!commands add !pp $(eval
+const responses = [
+`Your pp is -2 inches long! Oh no! It's retreated inside. Are you cold? Or afraid?`,
+`Your pp is -1 inch long! Oh no! It's retreated inside. Are you cold? Or afraid?`,
+`Your pp is 0 inches long! Oop, looks like you're smooth around the bend.`,
+`Your pp is 1 inch long! Just remember, it's not the size that matters.`,
+`Your pp is 2 inches long! Just remember, it's not the size that matters.`,
+`Your pp is 3 inches long! Tell people you're a grower, they'll believe that.`,
+`Your pp is 4 inches long!Tell people you're a grower, they'll believe that.`,
+`Your pp is 5 inches long! Nothing to write home about, but nothing to complain about either.`,
+`Your pp is 6 inches long! Nothing to write home about, but nothing to complain about either.`,
+`Your pp is 7 inches long! Finally, some decent hog around here.`,
+`Your pp is 8 inches long! Finally, some decent hog around here.`,
+`Your pp is 9 inches long! Be careful swinging that thing around, you could hurt someone!`,
+`Your pp is 10 inches long! Be careful swinging that thing around, you could hurt someone!`,
+`Your pp is 11 inches long! Only Diet Coke drinkers get this large.`,
+`Your pp is 12 inches long! Only Diet Coke drinkers get this large.`,
+`Your pp is 13 inches long! You're the same size as Sonny!`
+];
+
+responses[Math.floor(Math.random() * responses.length)];
+//)
